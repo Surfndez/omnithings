2d80930976d258f3d1b2cf86567992d6e30a5f73
ASUS Cloud <ASUSCloudIoT@users.noreply.github.com>
2017-10-16 04:00:46 -0500

    Fix encodeURIComponent Problem

--- SDK/src/js/authorization.js
+++ SDK/src/js/authorization.js
@@ -2,14 +2,16 @@
 
 var CryptoJS = require("crypto-js");
 
-function composeAuthorization(progKey){
-	
+var ran = function(){return Math.random().toString(36).substring(7).substr(0,5);}
+var composeAuthorization = function(progKey){
+					
 	var timestamp = Math.floor(Date.now() / 1000);	
-	var nonce = randomString(20);
+	var nonce = ran()+ran()+ran()+ran();
 	var signature = "nonce=" + nonce + "&signature_method=HMAC-SHA1&timestamp=" + timestamp;
-	
-	signature = encodeURI(signature);	
+					
+	signature = encodeURIComponent(signature);	
 	signature = CryptoJS.HmacSHA1(signature, progKey);
-	
+	signature = CryptoJS.enc.Base64.stringify(signature);
+	signature = encodeURIComponent(signature);
 	return 'nonce="' + nonce + '",signature_method="HMAC-SHA1",timestamp="' + timestamp + '",signature="' +signature +'"';
 }
